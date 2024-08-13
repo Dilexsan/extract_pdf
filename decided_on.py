ed1f90281388c208415f2d46b551db8bd8f5468d
Dilexsan <120619547+Dilexsan@users.noreply.github.com>
2024-08-13 20:19:09 +0530

    change the pattern on decided_on

--- decided_on.py
+++ decided_on.py
@@ -9,27 +9,39 @@ def read_pdf_file(file):
     with open(file, "rb") as file_handle:
         text = extract_text(file_handle)
     text = re.sub(r"\s+", " ", text)
-
+    
     return text
 
 
 def get_decided_on(text):
-
-    pattern1 = r"(?:Decided on|DECIDED ON |Decided On)\s*[:](\s*.*?\d{4})"
-    pattern2 = r"(?:Judgment On|JUDGMENT ON)\s*[:](\s*\d{2}.*?\d{4})"
+    text = text.lower()
+    # pattern1 = r"(?:Decided on|DECIDED ON |Decided On)\s*[:](\s*\d{2,4}-?\/?.?\d{2}-?\/?.?\d{2,4})"
+    # pattern2 = r"(?:Judgment On|JUDGMENT ON)\s*[:](\s*\d{2,4}-?\/?.?\d{2}-?\/?.?\d{2,4})"
+    pattern1 = r"(?:decided on|judgment on|judgment delivered on)\s*[:]\s*(\d{1,4}[-/.]\d{1,2}[-/.]\d{1,4})"
+    pattern2 = r"(?:decided on|judgment on|judgment delivered on)\s*[:]\s*(\d{1,2}.*?\d{4})"
     matches1 = re.findall(pattern1, text)
-    matches1 = re.findall(pattern2, text)
-
-
-    matches1 = re.findall(pattern1, text)
-    if matches1:
-        return matches1
-
     matches2 = re.findall(pattern2, text)
-    if matches2:
-        return matches2
 
-    return ["0"]
+    # dates = []
+    # all_dates = []
+    # if matches1:
+    #     dates.append(matches1[0])
+    #     return dates
+    # else:
+    #     matches2 = re.findall(pattern2, text)
+    #     dates.append(matches2[0])
+    #     return dates
+    
+    dates = []
+    if matches1:
+        dates.append(matches1[0])
+        
+    elif matches2:  
+        dates.append(matches2[0])
+    else:
+        dates.append("0")
+       
+    return dates
 
 
 def find_all_pdfs(dir_path):
@@ -51,7 +63,7 @@ def main(dir_path):
             continue
 
     # Write judge names to CSV
-    with open("august.csv", "w", newline="") as csvfile:
+    with open("decided.csv", "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["Judgment date"])
         for judgement_date in judgement_dates:
@@ -61,5 +73,5 @@ def main(dir_path):
 
 
 if __name__ == "__main__":
-    dir_path = "D:\d\Intern_works\Automation\extract_pdf\ca_cases_new_website\ca_cases_2024\\july"
+    dir_path = "D:\d\Intern_works\Automation\extract_pdf\ca_cases_new_website\ca_cases_2024\\august"
     main(dir_path)
